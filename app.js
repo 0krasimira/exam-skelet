2cbc81040a6f3622d6553a397fb1413577ccf0ea
Irina <134360973+irinaisheree@users.noreply.github.com>
2023-11-30 12:32:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-import page from "./"
+import page from "./node_modules/page/page.mjs"
+import { render } from "./node_modules/lit-html/lit-html.js"
+ 
+import { userService } from "./src/userService.js"
+import { userHelper } from "./src/userHelper.js"
+ 
+const root = /*add root here*/
+const userA = /*add user nav*/
+const guestA = /*add guest nav*/
+ 
+page(decorationContext)
+page("/logout", logout)
+ 
+page.start()
+updateNav()
+ 
+async function logout() {
+    await userService.logout()
+    updateNav()
+    goTo("/")
+}
+ 
+function renderer(template){
+    render(template, root)
+}
+ 
+function updateNav(){
+    const userData = userHelper.getUserData()
+ 
+    if(userData){
+        userA.forEach(a => a.style.display = "inline");
+        guestA.forEach(a => a.style.display = 'none')
+    } else {
+        userA.forEach(a => a.style.display = "none");
+        guestA.forEach(a => a.style.display = 'inline')
+    }
+}
+ 
+function goTo(path) {
+    page.redirect(path)
+}
+ 
+function decorationContext(ctx, next) {
+    ctx.render = renderer;
+    ctx.updateNav = updateNav;
+    ctx.goTo = goTo;
+ 
+    next()
+}
